9d91f5d8674842671d064bfe99c6253d8cc5fa80
noellekh <63249943+noellekh@users.noreply.github.com>
2021-08-10 19:41:10 +0200

    creation formulaire

---
+++ src/ledform/forms.py
@@ -0,0 +1,24 @@
+from django import forms 
+from .models import Product
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = [
+            'title',
+            'description',
+            'price'
+        ]
+class RawProductForm(forms.Form):
+    title = forms.CharField(label="", widget=forms.TimeInput(attrs={"placeholder": "your title"}))
+    description =  forms.CharField(
+                            required=False,
+                            widget=forms.Textarea(
+                            attrs={
+                                "class": "new_class_name two",
+                                "id": "my_id",
+                                "rows": 20,
+                                "cols": 120
+                            })
+    )
+    price = forms.DecimalField()
