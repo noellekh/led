8e7638540247c95ed037a9143af3e8268dcf417c
noellekh <63249943+noellekh@users.noreply.github.com>
2021-08-10 19:43:29 +0200

    liens vers les différentes pages

--- src/ledpro/urls.py
+++ src/ledpro/urls.py
@@ -16,11 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view, contact_view
-from ledform.views import product_detail_view
+from ledform.views import product_detail_view, product_create_view, product_list_view
+
 
 urlpatterns = [
     path ('', home_view, name='home'),
     path ('contact/', contact_view),
     path('admin/', admin.site.urls),
-    path ('product/',product_detail_view )
+    path ('product/',product_detail_view ),
+    path ('create/', product_create_view),
+    path('products/{int:id}/', product_detail_view, name='product-detail'),
+    path ('products/', product_list_view, name='product-list')
 ]
